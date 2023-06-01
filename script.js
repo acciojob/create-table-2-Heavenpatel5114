ca65a133ee214e4cd43a333765f7e2e6a0be1d4a
Heavenpatel5114 <113304844+Heavenpatel5114@users.noreply.github.com>
2023-06-01 12:32:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 function createTable() {
-    //Write your code here
-  
+  // Prompt for the number of rows
+  const numRows = parseInt(prompt("Input number of rows"));
+
+  // Prompt for the number of columns
+  const numCols = parseInt(prompt("Input number of columns"));
+
+  // Get the table element
+  const table = document.getElementById('myTable');
+
+  // Clear the existing table
+ // table.innerHTML = '';
+
+  // Create the table rows and columns
+  for (let i = 0; i < numRows; i++) {
+    const row = table.insertRow();
+    for (let j = 0; j < numCols; j++) {
+      const cell = row.insertCell();
+      cell.textContent = `Row-${i} Column-${j}`;
+    }
+  }
 }
